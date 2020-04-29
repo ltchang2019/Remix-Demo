3bf579d05eea3d8683f424ccc7d78fc94789a59b
Luke Tchang <ltchang@stanford.edu>
2020-04-28 22:58:58 -0700

    Initial Commit

---
+++ Remix/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Remix
+//
+//  Created by Luke Tchang on 4/28/20.
+//  Copyright © 2020 Luke Tchang. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
